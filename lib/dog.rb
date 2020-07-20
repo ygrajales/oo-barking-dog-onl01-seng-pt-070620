5fc3505c5d21b2ce1e5276b92d151257f5c91057
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-20 23:53:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Dog 
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
